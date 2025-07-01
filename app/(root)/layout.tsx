c07c5b93d6896647141d2412ffbfc5b838ad611c
Udbhav <udbhavkumar54@gmail.com>
2025-07-01 13:57:08 +0530

    Home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import { ReactNode } from 'react'
 
 const RootLayout = ({ children } : { children: ReactNode}) => {
   return (
-    <div>{ children }</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32}/>
+          <h2 className='text-primary-100'>Preply</h2>
+        </Link>
+      </nav>
+      { children }
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "Preply",
   description: "An AI-powered platform for preparing for mock interviews.",
 };
 
